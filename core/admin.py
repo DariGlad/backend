c605bc5c36334e3c11576114b6bb3e05d26b6fb7
dariglad <DariGlad>
2023-02-20 15:59:37 +0500

    add field company for user

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from core.models import User
 
@@ -11,6 +13,7 @@ class UserAdmin(UserAdmin):
         'first_name',
         'last_name',
         'email',
+        'company_link'
     )
     list_display_links = ('username',)
     readonly_fields = ('last_login', 'date_joined',)
@@ -26,9 +29,40 @@ class UserAdmin(UserAdmin):
              "first_name",
              "last_name",
              "email",
+             "company"
          )}),
         ('Права доступа',
          {"fields": (
              "is_staff",
          )}),
     )
+    fieldsets = (
+        (None,
+         {"fields": (
+             "username",
+             "password",
+         )}),
+        ('Персональная информация',
+         {"fields": (
+             "first_name",
+             "last_name",
+             "email",
+             "company"
+         )}),
+        ('Права доступа',
+         {"fields": (
+             "is_staff",
+             "is_active",
+             "groups",
+             "user_permissions"
+         )}),
+    )
+
+    @admin.display(description='Компания')
+    def company_link(self, obj):
+        if obj.company:
+            link = reverse(
+                'admin:company_company_change',
+                args=(obj.company.id,)
+            )
+            return mark_safe(u'<a href="{0}">{1}</a>'.format(link, obj.company))
--- core/models.py
+++ core/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+    company = models.ForeignKey(
+        'company.Company',
+        on_delete=models.CASCADE,
+        related_name='employees',
+        null=True,
+        verbose_name='Компания'
+    )
+
     def __str__(self):
         return self.username
